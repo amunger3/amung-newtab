6570af05b857a9e78d0abdf0c2f47778a1986b34
Alex Munger <munger.alex@gmail.com>
2020-12-01 16:43:00 -0500

    undid

--- src/js/pre/app.js
+++ src/js/pre/app.js
@@ -3,13 +3,3 @@ import Icons from 'uikit/dist/js/uikit-icons';
 
 // loads the Icon plugin
 UIkit.use(Icons);
-
-
-// Fetch JSON initialization
-const fetchJson = require('../vendor/fetch-json.js');
-
-const url = 'https://api.sofascore.com/api/v1/sport/football/trending-top-players';
-const handleData = (data) =>
-    toJson(data);
-var ttp = fetchJson.get(url).then(handleData);
-console.log(ttp);
